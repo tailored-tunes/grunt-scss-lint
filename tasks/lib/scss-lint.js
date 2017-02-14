150c9ef664055c48adb10f2c3ae25d04d4cd8251
meza <meza@meza.hu>
2017-02-14 18:23:08 +0000

    -.-

--- tasks/lib/scss-lint.js
+++ tasks/lib/scss-lint.js
@@ -90,7 +90,7 @@ exports.init = function (grunt) {
       cwd: process.cwd(),
       env: env
     }, function (err, results, code) {
-      if (err && err.code > 0) {
+      if (err && err.code > 2) {
         if (err.code === 127) {
           grunt.log.errorlns('1. Please make sure you have ruby installed: `ruby -v`');
           grunt.log.errorlns('2. Install the `scss-lint` gem by running:');
